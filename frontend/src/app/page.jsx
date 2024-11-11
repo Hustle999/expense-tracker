8c051eb30366483e16555b9ca410adb23daa1445
Төгөлдөр Гантөмөр <24LP4099@G401-CC0106.local>
2024-11-11 20:56:40 +0800

    link

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,3 +3,5 @@ import { LoginPage } from "./components/login/LoginPage";
 export default function Home() {
   return <LoginPage />;
 }
+
+// https://expense-tracker-3l3k3ofen-hustle999s-projects.vercel.app
